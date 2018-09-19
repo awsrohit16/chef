3b5d5aafaa03eb73201c1ab2fb5ca9266f66c5c8
Rohit Ranjan <rohitr@one.com>
2018-09-19 16:56:50 +0530

    added timers

--- onecom-sysops/recipes/default.rb
+++ onecom-sysops/recipes/default.rb
@@ -12,12 +12,52 @@ node.default['nginx']['http_options']['gzip_min_length']	= {'disabled' => true }
 
 
 
-cookbook_file '/etc/nginx/sites-available/default' do
-  source 'default'
-  owner 'root'
-  group 'root'
-  mode '0644'
-  action :create
-  notifies :reload, 'service[nginx]', :delayed
-end
+template '/etc/nginx/sites-available/default' do
+       source 'default.erb'
+       owner 'root'
+       group 'root'
+       mode  0644
+       notifies :reload, 'service[nginx]', :delayed
+end  
+
+
+include_recipe 'onecom-timers::default'
+
+node.default['onecom']['timers']['jobs']['metrics-poll'] = {
+  'calendar'    => '*:*:0/30',
+  'accuracy'    => '10m'
+  'persistent'  => 'true'
+  'command' => '/usr/local/bin/get-metrics-data-webpods.pl',
+}
+
+node.default['onecom']['timers']['jobs']['abuse-notify-summary'] = {
+  'calendar'    => '*-*-1,15 08:00:00',
+  'accuracy'    => '10m'
+  'persistent'  => 'true'
+  'command' => '/usr/local/bin/opsutils-watch-report POST sendnotifysummary/systems/15',
+}
+
+node.default['onecom']['timers']['jobs']['watchreport-ops-day'] = {
+  'calendar'    => '10,17:00',
+  'accuracy'    => '10m'
+  'command' => '/usr/local/bin/opsutils-watch-report POST sendmail/ops-in/7.5',
+}
+
+node.default['onecom']['timers']['jobs']['watchreport-systems'] = {
+  'calendar'    => '16:30',
+  'accuracy'    => '10m'
+  'command' => '/usr/local/bin/opsutils-watch-report POST sendmail/systems/24',
+}
+
+node.default['onecom']['timers']['jobs']['watchreport-secondlvl'] = {
+  'calendar'    => '05:30',
+  'accuracy'    => '10m'
+  'command' => '/usr/local/bin/opsutils-watch-report POST sendmail/2ndlevel/24',
+}
+
+node.default['onecom']['timers']['jobs']['watchreport-ops-night'] = {
+  'calendar'    => '02:00',
+  'accuracy'    => '10m'
+  'command' => '/usr/local/bin/opsutils-watch-report POST sendmail/ops-in/9',
+}
 
